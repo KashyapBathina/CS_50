e4a841d3c85fa285018e80ab3469cde4af3a6388
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-08 23:56:29 +0000

    Tue, Mar 8, 2022, 11:56 PM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -8,11 +8,4 @@ int main(void)
 {
     int answer = get_int("How tall is the pyramid?  ");
     printf("Height = %i\n", answer);
-    {
-        for (int i > 8; i++)
-        {
-            printf("what\n?");
-        }
-        printf("what\n?");
-    }
 }
