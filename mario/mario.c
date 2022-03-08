748a5a2d1073b8aa57f0747a2025eb5d94088922
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-08 23:44:04 +0000

    Tue, Mar 8, 2022, 11:44 PM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int number(void)
+void number(void)
 
-int main(void)
+int main(void);
+
 {
     int answer = get_int("How tall is the pyramid?  ");
     printf("Height = %i\n", answer);
-    {
-        int n;
-        do
-        {
-         n = get_int("How tall is the pyramid?  ");
-        }
-        while (n > 8);
-
-        for ()
-    }
 }
+{
+void number(void);
+{
+    int answer = get_int("How tall is the pyramid?  "
+}
+}
+{
+    int n;
+    do
+    {
+    n = get_int("How tall is the pyramid?  ");
+    }
+    while (n > 8);
+
+    for (n = 1; n <= 8; i++)
+    }
