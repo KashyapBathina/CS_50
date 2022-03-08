f34ff701b82ac983354db56fb22c7e2636a743f9
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-08 00:50:15 +0000

    Tue, Mar 8, 2022, 12:50 AM +00:00

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,8 +1,8 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    string answer = get_string("What's your name? ");
+    string answer = get_string("What's your name?  ");
     printf("hello, %s\n", answer);
 }
