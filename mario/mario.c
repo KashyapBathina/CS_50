a8c4da3f1d6e809ff58500f36d32303672b5854a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-09 23:31:18 +0000

    Wed, Mar 9, 2022, 11:31 PM +00:00

--- mario/mario.c
+++ mario/mario.c
@@ -19,6 +19,7 @@ int main(void)
             {
                 if ( i + j < n - 1)
                     printf(" ");
+                else
                 printf("#");
             }
         // next row
