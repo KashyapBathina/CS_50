4513550eb7723cf0e7dcbb6f4b054cb932900329
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-10 01:08:14 +0000

    Thu, Mar 10, 2022, 1:08 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -12,25 +12,25 @@ int main(void)
 
     int coins = 0;
 
-    while (int cents > 25);
+    while(cents > 25);
         {
             cents = cents - 25;
             coins ++;
         }
-    while (int cents > 10);
+    while(cents > 10);
         {
-            cents = cents - 10
-            coins++
+            cents = cents - 10;
+            coins++;
         }
-    while (int cents > 5);
+    while(cents > 5);
         {
-            cents = cents - 5
-            coins++
+            cents = cents - 5;
+            coins++;
         }
-    while (int cents > 1);
+    while(cents > 1);
         {
-            cents = cents - 1
-            coins++
+            cents = cents - 1;
+            coins++;
         }
 
         printf("You will need at least %i", coins);
