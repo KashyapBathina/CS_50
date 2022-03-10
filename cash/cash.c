be8df57ec52db07e5c1ca4ac762b75565a1d297e
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-10 01:05:14 +0000

    Thu, Mar 10, 2022, 1:05 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -1,66 +1,37 @@
-#include <cs50.h>
 #include <stdio.h>
-
-int get_cents(void);
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
+#include <cs50.h>
 
 int main(void)
 {
-    // Ask how many cents the customer is owed
-    int cents = get_cents();
+    int cents;
+    do
+    {
+        cents = get_int("Enter your change: ");
+    }
+    while (cents <= 0);
 
-    // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+    int coins = 0;
 
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+    while (int cents > 25);
+        {
+            cents = cents - 25;
+            coins ++;
+        }
+    while (int cents > 10);
+        {
+            cents = cents - 10
+            coins++
+        }
+    while (int cents > 5);
+        {
+            cents = cents - 5
+            coins++
+        }
+    while (int cents > 1);
+        {
+            cents = cents - 1
+            coins++
+        }
 
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
-
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
-    // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
-
-int get_cents(void)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_quarters(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_dimes(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_nickels(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_pennies(int cents)
-{
-    // TODO
-    return 0;
+        printf("You will need at least %i", coins);
 }
