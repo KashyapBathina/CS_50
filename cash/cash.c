930eb5c6add49e74c2d6976a7d369ccb8c4665f9
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-10 01:11:15 +0000

    Thu, Mar 10, 2022, 1:11 AM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -12,22 +12,22 @@ int main(void)
 
     int coins = 0;
 
-    while(cents >= 25);
+    while(cents >= 25)
         {
             cents = cents - 25;
             coins ++;
         }
-    while(cents >= 10);
+    while(cents >= 10)
         {
             cents = cents - 10;
             coins++;
         }
-    while(cents >= 5);
+    while(cents >= 5)
         {
             cents = cents - 5;
             coins++;
         }
-    while(cents >= 1);
+    while(cents >= 1)
         {
             cents = cents - 1;
             coins++;
