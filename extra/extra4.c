b373791c65224de3439154d85e23980b1480c20a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-18 23:48:02 +0000

    Fri, Mar 18, 2022, 11:48 PM +00:00

--- extra/extra4.c
+++ extra/extra4.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
-float average(int length, int array[]);
 
 int main(void)
 {
     //getting number of scores
     int n = get_int("How many scores: ");
-
     int scores[n];
+
 
     //promting for scores
     for (int i = 0; i < n; i++)
@@ -17,19 +16,15 @@ int main(void)
     }
 
 
-    //printing average
-    float average(int length, int array[]);
-    printf ("Average: %f\n", average);
-
-}
-
-float average(int length, int array[])
-{
     //average
     int sum = 0;
-    for(int i = 0; i < length; i++)
+    for(int i = 0; i < n; i++)
     {
         sum = sum + array[i];
     }
     return sum / n;
+
+
+    //printing average
+    printf ("Average: %f\n", sum);
 }
