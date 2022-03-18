8038891ef6f964670dfb77759f9a21c76c9937ab
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-18 22:52:07 +0000

    Fri, Mar 18, 2022, 10:52 PM +00:00

--- extra/extra4.c
+++ extra/extra4.c
@@ -5,13 +5,13 @@ int main(void)
 {
     int n = get_int("How many scores: ");
 
-    int scores[n];
+    float scores[n];
 
     for (int i = 0; i < n; i++)
     {
         scores[i] = get_int("Score: ");
     }
 
-    printf ("Average: %f\n", (scores[0] + scores[1] + scores[2] / 3.0 ));
+    printf ("Average: %f\n", (scores[0] + scores[1] + scores[2]) / n);
 
 }
