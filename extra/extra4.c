7b13bed33ee9f466df17bb56df583f00c747aa39
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-19 00:23:58 +0000

    Sat, Mar 19, 2022, 12:23 AM +00:00

--- extra/extra4.c
+++ extra/extra4.c
@@ -22,7 +22,7 @@ int main(void)
         sum = sum + scores[i];
     }
 
-    float average = sum / n;
+    float average = (float) sum / (float) n;
 
     //printing average
     printf("%f\n", average);
