24560bfdd2e4ee953cede65fc0d3f3b96b8439ba
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-20 22:46:54 +0000

    Sun, Mar 20, 2022, 10:46 PM +00:00

--- extra/extra5.c
+++ extra/extra5.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+
+
+int main(void)
+{
+    string names[] = {"Raj", "Prameela"};
+    string numbers[] = {"650-740-2062", "650-538-4948"};
+
+    for(int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], "Raj") == 0)
+        
+    }
+}
