dfc0e3755c0699ec4a58d6b477fecb432b57c638
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-20 22:53:55 +0000

    Sun, Mar 20, 2022, 10:53 PM +00:00

--- extra/extra5.c
+++ extra/extra5.c
@@ -11,13 +11,13 @@ person;
 
 int main(void)
 {
-    person people[2]
+    person people[2];
 
-    people[0].name = "Raj"
-    people[0].numbers = "650-740-2062"
+    people[0].name = "Raj";
+    people[0].number = "650-740-2062";
 
-    people[1].name = "Prameela"
-    people[1].numbers = "650-538-4948"
+    people[1].name = "Prameela";
+    people[1].number = "650-538-4948";
 
     for(int i = 0; i < 2; i++)
     {
