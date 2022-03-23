7d59781cba9c150c153cae5ed8de074ea1a9a191
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-23 23:24:49 +0000

    Wed, Mar 23, 2022, 11:24 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -145,7 +145,10 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            
+            if (candidates[preferences[i][j].eliminated == false])
+            {
+                candidates[preferences[i][j]]
+            }
         }
     }
     if
