588a4c184efc556ad9856ac3784206a36a5062cd
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-03-23 23:40:28 +0000

    Wed, Mar 23, 2022, 11:40 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -83,7 +83,7 @@ int main(int argc, string argv[])
     }
 
     // Keep holding runoffs until winner exists
-    while (true)
+    while ( )
     {
         // Calculate votes given remaining candidates
         tabulate();
