a963d4093d780b089c6ff71f0b1fc9ddf3e10a1f
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-04-06 00:27:08 +0000

    Wed, Apr 6, 2022, 12:27 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -53,8 +53,8 @@ int main(int argc, char *argv[])
     {
         fwrite(buffer, sizeof(char), 512, output_file);
     }
-}
-free(filename);
-fclose(output_file);
-fclose(input_file);
+    }
+    free(filename);
+    fclose(output_file);
+    fclose(input_file);
 }
