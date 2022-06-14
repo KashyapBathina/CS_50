9d06bc187a815a68b012edd1ac870e315c7e1273
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-14 22:04:12 +0000

    Tue, Jun 14, 2022, 10:04 PM +00:00

--- extra/extra7.c
+++ extra/extra7.c
@@ -1,12 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
     char *i = get_string("i: ");
     char *j = get_string("j: ");
 
-    if (i == j)
+    if (strcmp(i,j) == 0)
     {
         printf("Same\n");
     }
