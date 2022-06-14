75dbbd271f548cc8ad7ac8bdfd677a2b1e4c4bc5
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-14 23:43:40 +0000

    Tue, Jun 14, 2022, 11:43 PM +00:00

--- extra/extra7.c
+++ extra/extra7.c
@@ -1,18 +1,21 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
 {
-    char *i = get_string("i: ");
-    char *j = get_string("j: ");
+    char *s = get_string("s: ");
 
-    if (strcmp(i,j) == 0)
-    {
-        printf("Same\n");
-    }
-    else
-    {
-        printf("Different\n");
-    }
+    char *t = malloc(strlen(s) + 1);
+
+    strcpy(t, s);
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
 }
