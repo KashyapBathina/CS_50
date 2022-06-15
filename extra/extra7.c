a924e349d93a32d414a29a06b9949b7eaf7581bd
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-15 23:47:11 +0000

    Wed, Jun 15, 2022, 11:47 PM +00:00

--- extra/extra7.c
+++ extra/extra7.c
@@ -5,17 +5,9 @@ int main(void)
 {
     int x = 1;
     int y = 2;
-    int *p = &x
+    int *p = &x;
     printf("x is %i, y is %i\n", x, y);
-    
+    *p = x + 2;
     printf("x is %i, y is %i\n", x, y);
 }
 
-
-int main()
-{
-   int a = 7, b ;
-   int *p; // Un-initialized Pointer
-   p = &a; // Stores address of a in ptr
-   b = *p; // Put Value at ptr in b
-}
