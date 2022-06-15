f7b217f3071bf2801761fa743a138bbbd5c35671
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-15 22:37:23 +0000

    Wed, Jun 15, 2022, 10:37 PM +00:00

--- extra/extra8.c
+++ extra/extra8.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void swap(int *a, int *b);
+void add(int *a, int *b);
 
 int main(void)
 {
@@ -8,13 +8,11 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(&x, &y);
+    add(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int *a, int *b)
+void add(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+    
 }
