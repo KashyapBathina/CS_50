24e095ee8f51e1c19548973dcd87af06549f0f12
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-16 23:49:06 +0000

    Thu, Jun 16, 2022, 11:49 PM +00:00

--- extra/extra7.c
+++ extra/extra7.c
@@ -3,11 +3,8 @@
 
 int main(void)
 {
-    int x = 1;
-    int y = 2;
-    int *p = &x;
-    printf("x is %i, y is %i\n", x, y);
-    *p = x + 2;
-    printf("x is %i, y is %i\n", x, y);
-}
+    string s = get_string("W: ");
+    char *t = &s;
 
+    printf("p%\n", s);
+    printf("p%\n", t);
