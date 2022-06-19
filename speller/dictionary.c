a6b65a16a69ceed1546a9aed4b1ca114633fbcf5
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-19 23:08:47 +0000

    Sun, Jun 19, 2022, 11:08 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -37,12 +37,6 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
-    {
-        return false;
-    }
-
     return false;
 }
 
