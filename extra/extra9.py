b3afea95e036a9043bc0c0e081aade25f33d3653
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 21:50:26 +0000

    Mon, Jun 20, 2022, 9:50 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -1,9 +1,10 @@
 from PIL import Image, ImageFilter
 from cs50 import get_string
 
+picture = get_string("What image do you want to filter (car.jpeg/mountain.jpeg/person.jpeg/house/jpeg)")
 input = get_string("What filter do you want (black and white/blur)")
 
-image = Image.open("extra9.jpeg")
+image = Image.open("picture")
 
 if input == ("black and white"):
     image = image.convert("L")
