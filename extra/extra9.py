e386b0e49971d7ee242f6546709509b0b9eec0c2
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 21:11:17 +0000

    Mon, Jun 20, 2022, 9:11 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -1,5 +1,16 @@
 from PIL import Image, ImageFilter
 
-before = Image.open("bridge.bmp")
+image = Image.open("extra.bmp")
+image = img.convert("L")
+image.save("after.bmp")
+
 after = before.filter(ImageFilter.BoxBlur(10))
 after.save("out.bmp")
+
+
+
+file = "C://Users/ABC/20.jpg"
+img = Image.open(file)
+
+img = img.convert("L")
+img.show()
