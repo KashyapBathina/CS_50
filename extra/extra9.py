551f597917a8ba6591ab6978ea5488f6fc62dd56
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 22:08:46 +0000

    Mon, Jun 20, 2022, 10:08 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -2,6 +2,8 @@ from PIL import Image, ImageFilter
 from cs50 import get_string
 import sys
 
+print("image: ", sys.argv)
+
 input = get_string("What filter do you want (black and white/blur)")
 
 image = Image.open(" ", sys.argv)
