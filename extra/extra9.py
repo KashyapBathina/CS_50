b9c1d38b66e916fc9f8a5f61fc4f6295fdcc80f7
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 21:14:25 +0000

    Mon, Jun 20, 2022, 9:14 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -1,16 +1,11 @@
 from PIL import Image, ImageFilter
 
-image = Image.open("extra.bmp")
+image = Image.open("extra9.jpeg")
 image = img.convert("L")
-image.save("after.bmp")
+image.save("after.jpeg")
 
-after = before.filter(ImageFilter.BoxBlur(10))
-after.save("out.bmp")
-
-
-
-file = "C://Users/ABC/20.jpg"
-img = Image.open(file)
-
-img = img.convert("L")
-img.show()
+// after = before.filter(ImageFilter.BoxBlur(10))
+// after.save("out.bmp")
+// img = Image.open(file)
+// img = img.convert("L")
+// img.show()
