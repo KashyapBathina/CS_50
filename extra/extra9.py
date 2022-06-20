45bdb7038820b2559a52af1da807eb4d6be95ecd
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 22:57:52 +0000

    Mon, Jun 20, 2022, 10:57 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -2,6 +2,9 @@ from PIL import Image, ImageFilter
 from cs50 import get_string
 import sys
 
+if argcv < 2 
+    print(""Usage: extra9.py [car.jpeg / house.jpeg / mountain.jpeg / person.jpeg\n")
+
 picture = sys.argv[1]
 
 input = get_string("What filter do you want (black and white/blur)")
