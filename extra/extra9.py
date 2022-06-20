f86674964eb2c94588127c7e2e3b2cdeb5ca2749
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-20 21:23:24 +0000

    Mon, Jun 20, 2022, 9:23 PM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -1,6 +1,14 @@
 from PIL import Image
+from cs50 import get_string
+
+input = get_string("What filter do you want (black and white/blur)")
 
 image = Image.open("extra9.jpeg")
-image = image.convert("L")
-image.save("after.jpeg")
 
+if input == black and white:
+    image = image.convert("L")
+    image.save("after")
+
+if input == blur:
+    image = image.filter(ImageFilter.BLUR)
+    image.save("after")
