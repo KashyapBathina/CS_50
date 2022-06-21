3020e20f451bec7be74c90420be9c387a8d56603
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-21 22:07:04 +0000

    Tue, Jun 21, 2022, 10:07 PM +00:00

--- extra/extra10.py
+++ extra/extra10.py
@@ -1,16 +1,32 @@
-def main():
-    height = get_height()
-    for i in range(height):
-        print("#")
+rom cs50 import get_string
 
-def get_height():
-    while True:
-        try:
-            n = int(input("Height: "))
-            if n > 0:
-                break
-        except ValueError:
-            print("That's not an integer!")
-    return n
+people = {
+    "Carter": "+1-617-495-1000",
+    "David": "+1-949-468-2750"
+}
 
-main()
+name = get_string("Name: ")
+if name in people:
+    number = people[name]
+    print(f"Number: {number[name]}")
+
+
+
+
+
+#def main():
+#    height = get_height()
+#    for i in range(height):
+#        print("#")
+
+#def get_height():
+#    while True:
+#        try:
+#            n = int(input("Height: "))
+#            if n > 0:
+#                break
+#        except ValueError:
+#            print("That's not an integer!")
+#    return n
+
+#main()
