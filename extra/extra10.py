d95740e6661e2430843416a34e1100c009b01eec
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-21 22:08:49 +0000

    Tue, Jun 21, 2022, 10:08 PM +00:00

--- extra/extra10.py
+++ extra/extra10.py
@@ -1,14 +1,14 @@
 rom cs50 import get_string
 
 people = {
-    "Carter": "+1-617-495-1000",
-    "David": "+1-949-468-2750"
+    "Home": "+1-650-740-2062",
+    "Kashyap": "+1-407-775-0765"
 }
 
 name = get_string("Name: ")
 if name in people:
-    number = people[name]
-    print(f"Number: {number[name]}")
+    # same things as number = people[name]
+    print(f"Number: {people[name]}")
 
 
 
