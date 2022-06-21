a8ab82f3214161ec90fcefcd38461b7e5173ba3f
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-21 00:55:13 +0000

    Tue, Jun 21, 2022, 12:55 AM +00:00

--- extra/extra9.py
+++ extra/extra9.py
@@ -1,26 +1,29 @@
 from PIL import Image, ImageFilter
 import sys
 
-picture = sys.argv[1].lower()
+def main():
+    picture = sys.argv[1].lower()
 
-if len(sys.argv) < 2:
-    print("Usage: extra9.py [car.jpeg / house.jpeg / mountain.jpeg / person.jpeg\n")
-    exit()
+    if len(sys.argv) < 2:
+        print("Usage: extra9.py [car.jpeg / house.jpeg / mountain.jpeg / person.jpeg\n")
+        exit()
 
-if not picture in ["car.jpeg", "house.jpeg", "mountain.jpeg", "person.jpeg"]:
-    print("Usage: extra9.py [car.jpeg / house.jpeg / mountain.jpeg / person.jpeg\n")
-    exit()
+    if not picture in ["car.jpeg", "house.jpeg", "mountain.jpeg", "person.jpeg"]:
+        print("Usage: extra9.py [car.jpeg / house.jpeg / mountain.jpeg / person.jpeg\n")
+        exit()
 
-picture = sys.argv[1]
+    input = input("What filter do you want (black and white/blur)")
+    editor()
 
-input = input("What filter do you want (black and white/blur)")
+def editor():
+    image = Image.open(f"{picture}")
 
-image = Image.open(f"{picture}")
+    if input == ("black and white"):
+        image = image.convert("L")
+        image.save(f"post {picture}")
 
-if input == ("black and white"):
-    image = image.convert("L")
-    image.save(f"post {picture}")
+    if input == ("blur"):
+        image = image.filter(ImageFilter.BLUR)
+        image.save(f"post {picture}")
 
-if input == ("blur"):
-    image = image.filter(ImageFilter.BLUR)
-    image.save(f"post {picture}")
+main()
