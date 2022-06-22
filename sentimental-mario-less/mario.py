08d129a1122f23e52ee45c97e2ef5d76699f286f
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 22:55:57 +0000

    Wed, Jun 22, 2022, 10:55 PM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,11 +1,12 @@
 # TODO
 from cs50 import get_int
 
+
 height = get_int("Height: ")
 if height < 0 or height > 9:
-    break
+    exit()
 
 for i in range(height):
     print((height - i) * " ", end = "")
-    print(height * "#")
+    print((height - 3) * "#")
 
