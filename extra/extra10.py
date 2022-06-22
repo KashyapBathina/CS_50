c32a8b03fd87bc770b7167ea5fd411b6140460da
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 23:09:42 +0000

    Wed, Jun 22, 2022, 11:09 PM +00:00

--- extra/extra10.py
+++ extra/extra10.py
@@ -39,6 +39,21 @@ os.remove("list.csv")
 
 
 
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+#from cs50 import get_int
+
+# # the while true functions repeats until you break
+#while True:
+#    height = get_int("Height: ")
+#    if height > 0 and height < 9:
+#        break
+
+#for i in range(height):
+#    print((height - 1 - i) * " ", end="")
+#    print((i + 1) * "#")
 
 
 
