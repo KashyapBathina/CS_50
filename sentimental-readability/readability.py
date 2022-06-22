90c1c1a03e287097ce8a2bc0a5fd8677a47e6f27
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 23:44:52 +0000

    Wed, Jun 22, 2022, 11:44 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,11 +1,23 @@
 # TODO
 from cs50 import get_string
 
+words = 0
+sentences = 0
+
 text = get_string("Text: ")
 
-for letters in text:
+for l in text:
     letters = len(text)
 
-for words in text:
-    for 
+for w in text:
+    if " " in text:
+        words += 1
+
+for s in text:
+    if "." or "!" or "?" in text:
+        sentences += 1
+
+print(letters)
+print(words)
+print(sentences)
 
