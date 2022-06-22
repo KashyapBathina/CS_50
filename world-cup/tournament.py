54d7362ee2f4863834a413d612aad5026ada9a99
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 21:01:30 +0000

    Wed, Jun 22, 2022, 9:01 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -18,10 +18,8 @@ def main():
     # TODO: Read teams into memory from file
     with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
-        for row in reader:
-            team = row["team"]
-            int rating = row["rating"]
-            teams.append(team, rating)
+        for team in reader:
+            team["rating"]
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
