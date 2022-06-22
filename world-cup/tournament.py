fa720d59ba906f53c0757f3849e76d3f7e17a705
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 20:55:28 +0000

    Wed, Jun 22, 2022, 8:55 PM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -20,7 +20,7 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             team = row["team"]
-            rating = row{"rating"}
+            rating = row["rating"]
             teams.append(team, rating)
 
     counts = {}
