de9db4cbb5eef66c5d0124254a4e787b968dd7c6
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-22 23:58:49 +0000

    Wed, Jun 22, 2022, 11:58 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -3,15 +3,18 @@ from cs50 import get_string
 
 
 text = get_string("Text: ")
-print (len(text))
 
-for w in text:
-    if " " in text:
-        words += 1
+for i in range(len(text))
+    if(text[i].isalpha()):
+        letters += 1
 
-for s in text:
-    if "." or "!" or "?" in text:
+    if (text[i].isspace()):
+        words +=1
+
+    if (text[i]"." or text[i] = "!" or text[i] = "?"):
         sentences += 1
+
+grade level = 0.0588 * (L) - 0.296 * (S) - 15.8
 
 
 
