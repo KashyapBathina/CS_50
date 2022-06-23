14b6798264e2d4a87543cdccd7ecb670d9dc5385
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-23 22:10:57 +0000

    Thu, Jun 23, 2022, 10:10 PM +00:00

--- extra/extra11.py
+++ extra/extra11.py
@@ -1,11 +1,26 @@
-pizzas = {
-    "cheese" : 9,
-    "pepperoni" : 10,
-    "vegetable" : 11,
-    "chicken" : 12
-}
 
-pizzas["meat"] = 14
 
-for pie, price in pizzas.items():
-    print(f"A whole {pie} pizza costs {price}")
+
+
+
+
+
+
+
+
+
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+
+#pizzas = {
+#    "cheese" : 9,
+#    "pepperoni" : 10,
+#    "vegetable" : 11,
+#    "chicken" : 12
+#}
+
+#pizzas["meat"] = 14
+
+#for pie, price in pizzas.items():
+#    print(f"A whole {pie} pizza costs {price}")
