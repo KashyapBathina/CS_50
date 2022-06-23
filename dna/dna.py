21344a51e3d2db3d7c04f3351c19da50f0863326
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-23 22:00:17 +0000

    Thu, Jun 23, 2022, 10:00 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -22,6 +22,7 @@ def main():
 
     # This results in "name" : 0
     for i in database[0]:
+        print(i)
     # where sequence is the full dna sequence and i is the subsequence like agat
         matches[i] = (longest_match(sequence, i))
 
