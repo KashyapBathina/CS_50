5517883fb065309c589f96f202ba95cdf69cedc9
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 21:57:53 +0000

    Sat, Jun 25, 2022, 9:57 PM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -4,3 +4,6 @@ JOIN movies ON stars.movie_id = movies.id AND people.name = "Johnny Depp" AND mo
 SELECT movies.title FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id AND  people.name = "Helena Bonham Carter")
+
+
+-- 
