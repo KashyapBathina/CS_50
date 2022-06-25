64fe45f739b21e2f143d78669a851b1c549ae777
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 00:17:15 +0000

    Sat, Jun 25, 2022, 12:17 AM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,3 +1,7 @@
+--SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10);
+
+
+
 SELECT COUNT(movies.id) FROM movies JOIN ratings ON movies.id=ratings.movie_id AND ratings.rating=10;
 
 
