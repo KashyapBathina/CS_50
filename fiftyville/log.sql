e7833ffcfc086f7e431195700163cf715be9ca05
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 23:11:56 +0000

    Sat, Jun 25, 2022, 11:11 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -5,7 +5,7 @@ WHERE month = 7 AND day = 28
 AND street = "Humphrey Street";
 
 
-SELECT name FROM people WHERE id IN (
+SELECT name FROM people WHERE
 SELECT crime_scene_reports.id FROM people
 JOIN crime_scene_reports ON people.id = crime_scene_reports.id
 AND crime_scene_reports.month = 7 AND crime_scene_reports.day = 28 AND crime_scene_reports.id = 295
