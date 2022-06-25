a705a7cc9dde89c2afa263972e4dcc3b76d1884a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 00:44:22 +0000

    Sat, Jun 25, 2022, 12:44 AM +00:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,1 +1,9 @@
-a
+SELECT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title = "Toy Story";
+
+
+SELECT name FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id AND movies.title = "Toy Story";
