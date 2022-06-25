fb0e5240fb1948fb4a00b42d2692e8109d8928da
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 22:49:45 +0000

    Sat, Jun 25, 2022, 10:49 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,11 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT description
+SELECT description, id
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28
 AND street = "Humphrey Street";
 
 
-SELECT name FROM people
+SELECT name, id FROM people
 JOIN crime_scene_reports ON people.id = crime_scene_reports.id
 AND crime_scene_reports.month = 7 AND crime_scene_reports.day = 28
 AND crime_scene_reports.street = "Humphrey Street";
