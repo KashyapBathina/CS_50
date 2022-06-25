8f12a1a601718d9cb152d95f900a2a3bdc09443b
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 00:06:58 +0000

    Sat, Jun 25, 2022, 12:06 AM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,5 +1,6 @@
-SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10);
-
-
-
 SELECT COUNT(movies.id) FROM movies JOIN ratings ON movies.id=ratings.movie_id AND ratings.rating=10;
+
+
+SELECT title FROM movies
+JOIN stars on movies.id = stars.movie_id
+JOIN people ON stars.person_id = people.id AND people.name = "Emma Stone";
