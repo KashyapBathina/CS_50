1a45ea7534f837f9eaf005d1a52cc21c161c0a40
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 01:12:54 +0000

    Sat, Jun 25, 2022, 1:12 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,14 +1,6 @@
-SELECT title FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-AND people.name = "Johnny Depp" AND people.name = "Helena Bonham Carter";
-
-
 SELECT movies.title FROM people
 JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE people.name = "Johnny Depp" AND movies.title IN(
+JOIN movies ON stars.movie_id = movies.id AND people.name = "Johnny Depp" AND movies.title IN(
 SELECT movies.title FROM people
 JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE people.name = "Helena Bonham Carter")
+JOIN movies ON stars.movie_id = movies.id AND  people.name = "Helena Bonham Carter")
