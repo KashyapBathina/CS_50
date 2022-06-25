27a5ab75ca5694b65d389836f4810104327769ac
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-25 00:18:44 +0000

    Sat, Jun 25, 2022, 12:18 AM +00:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,5 +1,5 @@
 --SELECT COUNT(title) FROM movies WHERE id IN (SELECT movie_id FROM ratings WHERE rating = 10);
-
+--in essence: picks movie ids where ratings is 10 and counts it in moviesid
 
 
 SELECT COUNT(movies.id) FROM movies JOIN ratings ON movies.id=ratings.movie_id AND ratings.rating=10;
