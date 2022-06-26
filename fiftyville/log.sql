a4599ff5b782d174f5184c5f3705409bef9c4e3d
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-26 00:30:27 +0000

    Sun, Jun 26, 2022, 12:30 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,16 +7,16 @@ WHERE day = "28" AND month = "7"
 AND transcript like "%bakery%";
 
 SELECT name, atm_transactions.amount
-  FROM people
-  JOIN bank_accounts
-    ON people.id = bank_accounts.person_id
-  JOIN atm_transactions
-    ON bank_accounts.account_number = atm_transactions.account_number
- WHERE atm_transactions.year = 2021
-   AND atm_transactions.month = 7
-   AND atm_transactions.day = 28
-   AND atm_transactions.atm_location = 'Leggett Street'
-   AND atm_transactions.transaction_type = 'withdraw';
+FROM people
+JOIN bank_accounts
+ON people.id = bank_accounts.person_id
+JOIN atm_transactions
+ON bank_accounts.account_number = atm_transactions.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_transactions.atm_location = 'Leggett Street'
+AND atm_transactions.transaction_type = 'withdraw';
 
 SELECT flights.id, full_name, city, flights.hour, flights.minute
 FROM airports
@@ -29,87 +29,10 @@ WHERE city = 'Fiftyville')
 AND flights.year = 2021
 AND flights.month = 7
 AND flights.day = 29
- ORDER BY flights.hour, flights.minute;
+ORDER BY flights.hour, flights.minute;
 
 
 
 
 
 
- crime_scene_reports
-    id INTEGER,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    street TEXT,
-    description TEXT,
-
-people
-    id INTEGER,
-    name TEXT,
-    phone_number TEXT,
-    passport_number INTEGER,
-    license_plate TEXT,
-
-interviews
-    id INTEGER,
-    name TEXT,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    transcript TEXT,
-
-atm_transactions
-    id INTEGER,
-    account_number INTEGER,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    atm_location TEXT,
-    transaction_type TEXT,
-    amount INTEGER,
-
-bank_accounts
-    account_number INTEGER,
-    person_id INTEGER,
-    creation_year INTEGER,
-
-airports
-    id INTEGER,
-    abbreviation TEXT,
-    full_name TEXT,
-    city TEXT,
-
-flights
-    id INTEGER,
-    origin_airport_id INTEGER,
-    destination_airport_id INTEGER,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    hour INTEGER,
-    minute INTEGER,
-
-passengers
-    flight_id INTEGER,
-    passport_number INTEGER,
-    seat TEXT,
-
-phone_calls
-    id INTEGER,
-    caller TEXT,
-    receiver TEXT,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    duration INTEGER,
-
-bakery_security_logs
-    id INTEGER,
-    year INTEGER,
-    month INTEGER,
-    day INTEGER,
-    hour INTEGER,
-    minute INTEGER,
-    activity TEXT,
-    license_plate TEXT,
