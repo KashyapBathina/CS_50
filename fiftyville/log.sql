077990c9cf8cd3b8a86a9b2fd68803f9f12b32cc
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-26 00:33:30 +0000

    Sun, Jun 26, 2022, 12:33 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,10 +2,12 @@
 SELECT description FROM crime_scene_reports
 WHERE day = "28" AND month = "7" AND street = "Humphrey Street";
 
+
 SELECT name, transcript FROM interviews
 WHERE day = "28" AND month = "7"
 AND transcript like "%bakery%";
 
+-- bank path
 SELECT name, atm_transactions.amount
 FROM people
 JOIN bank_accounts
@@ -18,6 +20,7 @@ AND atm_transactions.day = 28
 AND atm_transactions.atm_location = 'Leggett Street'
 AND atm_transactions.transaction_type = 'withdraw';
 
+-- flight path
 SELECT flights.id, full_name, city, flights.hour, flights.minute
 FROM airports
 JOIN flights
@@ -32,7 +35,30 @@ AND flights.day = 29
 ORDER BY flights.hour, flights.minute;
 
 
+SELECT passengers.flight_id, name, passengers.passport_number, passengers.seat
+  FROM people
+  JOIN passengers
+    ON people.passport_number = passengers.passport_number
+  JOIN flights
+    ON passengers.flight_id = flights.id
+ WHERE flights.year = 2021
+   AND flights.month = 7
+   AND flights.day = 29
+   AND flights.hour = 8
+   AND flights.minute = 20
+ ORDER BY passengers.passport_number;
 
 
-
-
+-- car license path
+SELECT name, bakery_security_logs.hour, bakery_security_logs.minute
+  FROM people
+  JOIN bakery_security_logs
+    ON people.license_plate = bakery_security_logs.license_plate
+ WHERE bakery_security_logs.year = 2021
+   AND bakery_security_logs.month = 7
+   AND bakery_security_logs.day = 28
+   AND bakery_security_logs.activity = 'exit'
+   AND bakery_security_logs.hour = 10
+   AND bakery_security_logs.minute >= 15
+   AND bakery_security_logs.minute <= 25
+ ORDER BY bakery_security_logs.minute;
