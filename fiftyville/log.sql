86e3d47fe500fefba3cd2798e700a09d703c7f1a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-06-26 00:28:22 +0000

    Sun, Jun 26, 2022, 12:28 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,7 +6,30 @@ SELECT name, transcript FROM interviews
 WHERE day = "28" AND month = "7"
 AND transcript like "%bakery%";
 
+SELECT name, atm_transactions.amount
+  FROM people
+  JOIN bank_accounts
+    ON people.id = bank_accounts.person_id
+  JOIN atm_transactions
+    ON bank_accounts.account_number = atm_transactions.account_number
+ WHERE atm_transactions.year = 2021
+   AND atm_transactions.month = 7
+   AND atm_transactions.day = 28
+   AND atm_transactions.atm_location = 'Leggett Street'
+   AND atm_transactions.transaction_type = 'withdraw';
 
+SELECT flights.id, full_name, city, flights.hour, flights.minute
+FROM airports
+JOIN flights
+ON airports.id = flights.destination_airport_id
+WHERE flights.origin_airport_id =
+(SELECT id
+FROM airports
+WHERE city = 'Fiftyville')
+AND flights.year = 2021
+AND flights.month = 7
+AND flights.day = 29
+ ORDER BY flights.hour, flights.minute;
 
 
 
