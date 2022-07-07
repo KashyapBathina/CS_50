9e2198cc2cddee52ba7b8a558831087a163e1957
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-07 23:40:56 +0000

    Thu, Jul 7, 2022, 11:40 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -29,6 +29,24 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
+# Time
+def time_now():
+    """HELPER: get current UTC date and time"""
+    now_utc = datetime.now(timezone.utc)
+    return str(now_utc.date()) + ' @time ' + now_utc.time().strftime("%H:%M:%S")
+
+def own_shares():
+    """Helper function: Which stocks the user owns, and numbers of shares owned. Return: dictionary {symbol: qty}"""
+    user_id = session["user_id"]
+    owns = {}
+    query = db.execute("SELECT symbol, shares FROM orders WHERE user_id = ?", user_id)
+    for q in query:
+        symbol, shares = q["symbol"], q["shares"]
+        owns[symbol] = owns.setdefault(symbol, 0) + shares
+    # filter zero-share stocks
+    owns = {k: v for k, v in owns.items() if v != 0}
+    return owns
+
 
 @app.after_request
 def after_request(response):
@@ -221,21 +239,4 @@ def sell():
             return apology("Your card declined", 403)
 
         db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", request.form.get("money"), id=session["user_id"]);
-
-
-def time_now():
-    """HELPER: get current UTC date and time"""
-    now_utc = datetime.now(timezone.utc)
-    return str(now_utc.date()) + ' @time ' + now_utc.time().strftime("%H:%M:%S")
-
-def own_shares():
-    user_id = session["user_id"]
-    owns = {}
-    query = db.execute("SELECT symbol, shares FROM orders WHERE user_id = ?", user_id)
-    for q in query:
-        symbol, shares = q["symbol"], q["shares"]
-        owns[symbol] = owns.setdefault(symbol, 0) + shares
-    # filter zero-share stocks
-    owns = {k: v for k, v in owns.items() if v != 0}
-    return owns
 
