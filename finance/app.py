a40be4296b6dc0206804d00dee68ed0b0561ec53
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-09 01:13:34 +0000

    Sat, Jul 9, 2022, 1:13 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -222,14 +222,14 @@ def sell():
 
         cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]['cash']
 
-        quote = lookup(request.form.get("stock"))
+        quote = lookup(request.form.get("symbol"))
 
-        remaining = cash + quote["price"] * request.form.get("shares")
+        remaining = cash + quote["price"] * int(request.form.get("shares"))
 
         db.execute("UPDATE users SET cash = ? WHERE id = ?", remaining, session["user_id"])
 
         db.execute("INSERT INTO orders (user_id, symbol, shares, price, timestamp) VALUES (?, ?, ?, ?, ?)",
-        session["user_id"], request.form.get("stock"), request.form.get("shares"), quote["price"], time_now())
+        session["user_id"], request.form.get("symbol"), request.form.get("shares"), quote["price"], time_now())
 
         return redirect("/")
 
