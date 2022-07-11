ff9036a6b5299f2cf537a5269e95f5a0ba67f391
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-11 00:53:11 +0000

    Mon, Jul 11, 2022, 12:53 AM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 
 
 # Configure application
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///gradebook.db")
+db = SQL("sqlite:///darkmoon.db")
 
 @app.after_request
 def after_request(response):
