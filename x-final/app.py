e36679148a87fb67cc50f7f07d29c00987a5275c
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-11 00:18:06 +0000

    Mon, Jul 11, 2022, 12:18 AM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -53,8 +53,8 @@ def login():
     if request.method == "POST":
 
         # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
+        if not request.form.get("email"):
+            return apology("must provide email adress", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
@@ -65,7 +65,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid email and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
