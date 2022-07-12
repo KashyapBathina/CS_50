a53c4f550c339b672c1f8383f2af0b78341660f5
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-12 23:33:16 +0000

    Tue, Jul 12, 2022, 11:33 PM +00:00

--- lorem.py
+++ lorem.py
@@ -1,23 +1,25 @@
 from trycourier import Courier
 import string
-import secrets
+import random
 
 client = Courier(auth_token="Courier_Authentication_Token")
 
 name = input("name: ")
 email = input("email: ")
-code = ''.join(random.choice(letters) for i in range(10))
+code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+print(code)
 
 
 response = client.send(
     event="Registration",
     recipient="{email}",
     profile={
-        "name": "{name}",
-        "name": "{code}",
+        "recipient": "{email}",
     },
     data={
-        "Loredm Ipsum": "dolor sit amet"
+        "name": "{name}",
+        "code": "{code}",
     },
 )
 print("done")
