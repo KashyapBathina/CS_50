14b7e1dcdfdb80480805237d0b2f5282786d5093
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-12 22:48:59 +0000

    Tue, Jul 12, 2022, 10:48 PM +00:00

--- pl.py
+++ pl.py
@@ -1,7 +1,4 @@
-import smtplib
-from email.message import EmailMessage
-from email.headerregistry import Address
-import random
+from trycourier import Courier
 
 
 name = input("what is your name: ")
@@ -11,7 +8,18 @@ email = input("what is your email adress: ")
 hash = random.getrandbits(128)
 print("hash value: %032x" % hash)
 
-“data”: {
-    “name”: {name}
-    "code": {"%032x", %hash }
+client = Courier(auth_token="dk_prod_MAPAZZ24RRMQ7CGQ5VR7MX6051R8")
+
+response = client.send(
+    event="your-notification-id" #Your notification ID from Courier
+    recipient="your-recipient-id" #Usually your system's User ID
+    profile={
+        "email": "user@example.com" #The recipient’s email address
+    },
+    data={
+        “name”: {name}
+        "code": {"%032x", %hash }
     }
+)
+
+print(response['messageId'])
