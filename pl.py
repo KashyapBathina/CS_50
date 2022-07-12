edd2d8d9606f7d200f03e56a8006b7caa50ede01
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-12 23:03:17 +0000

    Tue, Jul 12, 2022, 11:03 PM +00:00

--- pl.py
+++ pl.py
@@ -1,14 +1,27 @@
 from trycourier import Courier
 
-client = Courier(auth_token="pk_prod_ZN043V85VAM138K22DMK8G8Y2F8Y")
+client = Courier(auth_token="pk_prod_ZN043V85VAM138K22DMK8G8Y2F8Y
+") #or set via COURIER_AUTH_TOKEN env var
 
 resp = client.send(
-  event="courier-quickstart",
-  recipient="aman@courier.com",
-  data={
-    "favoriteAdjective": "awesomeness"
-  },
-  profile={
-    "email": "aman@courier.com"
-  }
+    event="your-event-id",
+    recipient="your-recipient-id",
+    profile={
+        "email": "example@example.com",
+        "phone_number": "555-867-5309"
+    },
+    data={
+      "world": "Python!"
+    }
 )
+print(resp['messageId'])
+
+
+
+
+
+
+
+
+
+
