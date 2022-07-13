b4267f707d29e2cf755698a89214b05479364783
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-13 20:56:25 +0000

    Wed, Jul 13, 2022, 8:56 PM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -129,7 +129,7 @@ def register():
         organization = request.form.get("organization")
         number = request.form.get("number")
 
-        logging.info(f'{variety}')
+        if variety == "teacher"
 
         if not email or not password or not confirmation or not variety or not school or not first or not last or not variety or not school or not role or not organization or not number:
             return apology("must fill in all fields", 400)
