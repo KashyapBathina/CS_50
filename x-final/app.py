d8ef75eed7ec7a7402476d4a3e9fb58cb8e8fcd8
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-14 19:29:13 +0000

    Thu, Jul 14, 2022, 7:29 PM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -187,10 +187,13 @@ def verification():
         usercode = request.form.get("usercode")
         email = request.form.get("email")
         password = request.form.get("password")
+        variety = request.form.get("variety")
+        first = request.form.get("first")
+        last = request.form.get("last")
+        name = first last
 
         if str(gcode) == str(usercode):
-            result = db.execute("INSERT INTO users (email, hash) VALUES(?, ?)", email, generate_password_hash(password))
-
+            result = db.execute("INSERT INTO users (email, hash, name, type) VALUES(?, ?)", email, generate_password_hash(password))
             session["user_id"] = result
 
             return redirect("/index")
