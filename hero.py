7251a8489fcf86d33493b231cc5eb8d00d02f672
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-14 21:07:56 +0000

    Thu, Jul 14, 2022, 9:07 PM +00:00

--- hero.py
+++ hero.py
@@ -10,15 +10,26 @@ email = input("email: ")
 
 code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
-resp = client.send_message(
+resp = client.send(
 message={
-    "to": {
-    "email": (email),
+    event="2VC65XG43QM5K5PJEV05YC46NCM9"
+    profile={
+        "email": (email)
     },
     "data": {
         "name": (name),
         "code": (code),
         },
-    "template": "2VC65XG43QM5K5PJEV05YC46NCM9"
     }
 )
+
+response = client.send(
+    event="2VC65XG43QM5K5PJEV05YC46NCM9" #Your notification ID from Courier
+    profile={
+        "email": (email) #The recipient’s email address
+    },
+    data={
+        "Lorem Ipsum": "dolor sit amet" #The message you wish to send
+    }
+)
+
