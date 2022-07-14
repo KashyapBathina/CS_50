6de40d7aab15d43f2810d2dc2d00bb01abe3771e
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-14 20:40:29 +0000

    Thu, Jul 14, 2022, 8:40 PM +00:00

--- hero.py
+++ hero.py
@@ -1,20 +1,25 @@
 from trycourier import Courier
+import string
+import random
 
 client = Courier(auth_token="dk_prod_MAPAZZ24RRMQ7CGQ5VR7MX6051R8")
 
 
-name = input
-        code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+name = input("name: ")
+email = input("email: ")
+code = "JCA"
 
-        resp = client.send_message(
-        message={
-            "to": {
-            "email": (email),
-            },
-            "data": {
-                "name": (first),
-                "code": (code),
-            },
-            "template": "2VC65XG43QM5K5PJEV05YC46NCM9"
-        }
-        )
+code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+
+resp = client.send_message(
+message={
+    "to": {
+    "email": (email),
+    },
+    "data": {
+        "name": (name),
+        "code": (code),
+        },
+    "template": "2VC65XG43QM5K5PJEV05YC46NCM9"
+    }
+)
