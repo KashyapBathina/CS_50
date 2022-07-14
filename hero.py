ec77ce58288da192e06ea90210dde06178a13b26
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-14 23:32:01 +0000

    Thu, Jul 14, 2022, 11:32 PM +00:00

--- hero.py
+++ hero.py
@@ -6,20 +6,20 @@ name = input("name: ")
 email = input("email: ")
 code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
-client = Courier(auth_token="dk_prod_MAPAZZ24RRMQ7CGQ5VR7MX6051R8")
+client = Courier(auth_token="pk_prod_3VNJBYM54EM107NQ0ZZ62Y7CRY67")
 
 resp = client.send_message(
-  message={
-    "to": {
-        "email": (email)
-    },
-    "template": "2VC65XG43QM5K5PJEV05YC46NCM9",
-    "data": {
-      "name": (name),
-      "code": (code),
-    },
-  }
-)
+            message={
+                "to": {
+                    "email": (email),
+                },
+                "template": "C4P4331NKF4G9MHBY4663SM7XVA3",
+                "data": {
+                    "name": (name),
+                    "code": (code),
+                },
+            }
+        )
 
 print(resp['requestId'])
 
