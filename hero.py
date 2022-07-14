792e5d32bd0bd534dd6eab107b5a8d252e102c57
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-14 21:44:41 +0000

    Thu, Jul 14, 2022, 9:44 PM +00:00

--- hero.py
+++ hero.py
@@ -1,16 +1,26 @@
 from trycourier import Courier
 
+
+code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+name = "kashyap"
+
+
 client = Courier(auth_token="Courier_Authentication_Token")
 
 response = client.send(
-    event="your-notification-id" #Your notification ID from Courier
-    recipient="your-recipient-id" #Usually your system's User ID
+    event="2VC65XG43QM5K5PJEV05YC46NCM9" #Your notification ID from Courier
     profile={
-        "email": "user@example.com" #The recipient’s email address
+        "email": (email), #The recipient’s email address
     },
     data={
-        "Lorem Ipsum": "dolor sit amet" #The message you wish to send
+        "name": (name),
+        "code": (code),
     }
+    "template": "2VC65XG43QM5K5PJEV05YC46NCM9"
+)
+
+print(response['messageId'])
+
 )
 
 print(response['messageId'])
