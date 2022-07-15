12a28a5b4b937faa431d7e1637dd6c20cbd7b846
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2022-07-15 20:57:32 +0000

    Fri, Jul 15, 2022, 8:57 PM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -44,6 +44,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+session_start()
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///darkmoon.db")
