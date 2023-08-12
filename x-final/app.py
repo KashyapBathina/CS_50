e6fd2e9c6f138aac7e9a585397bb29771296f8b1
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-08-12 23:55:59 +0000

    Sat, Aug 12, 2023, 11:55 PM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -313,7 +313,7 @@ def fgrading():
             db.execute("UPDATE students SET grade = ? WHERE studentname = ? AND classname = ? AND teacherid = ?", round(fgrade), i, classname.strip(), session["user_id"])
 
             #send email
-            client = Courier(auth_token="pk_prod_3VNJBYM54EM107NQ0ZZ62Y7CRY67")
+            client = Courier(auth_token="<pk_prod_3VNJBYM54EM107NQ0ZZ62Y7CRY67>")
             resp = client.send_message(
                 message={
                     "to": {
