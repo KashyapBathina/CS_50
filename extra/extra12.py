dd79d6996d77a789b968fcbd3838a90ebf6edf1e
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-08-13 00:15:57 +0000

    Sun, Aug 13, 2023, 12:15 AM +00:00

--- extra/extra12.py
+++ extra/extra12.py
@@ -1,4 +1,24 @@
 from trycourier import Courier
 
-client = Courier(auth_token="<auth_token>")
+client = Courier(auth_token="dk_prod_S04HTYNPFD4RWFHTGW3ADT6VSSHN")
 
+resp = client.send_message(
+  message={
+    "to": {
+      "email": "kbathina05@gmail.com",
+    },
+    "content": {
+      "title": "Welcome!",
+      "body": "Hello, World",
+    },
+    "data": {
+      "name": "Kashyap Bathina",
+    },
+    "routing": {
+      "method": "single",
+      "channels": ["email"],
+    },
+  }
+)
+
+print(resp['requestId'])
