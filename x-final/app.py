a865acbb01bbc3c19c158744b27c41e808b8cc68
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-08-19 23:53:28 +0000

    Sat, Aug 19, 2023, 11:53 PM +00:00

--- x-final/app.py
+++ x-final/app.py
@@ -150,12 +150,10 @@ def register():
         if len(db.execute('SELECT email FROM users WHERE email = ?', email)) > 0:
             return apology("email already in use", 400)
 
-
-        # emailing through courier
         code = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
 
         client = Courier(auth_token="dk_prod_S04HTYNPFD4RWFHTGW3ADT6VSSHN")
-
+        
         resp = client.send_message(
             message={
                 "to": {
