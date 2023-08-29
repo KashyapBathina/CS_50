6e4b5bc9deec3cc8791c161e1e7280148959a1a8
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-08-29 19:19:06 +0000

    Tue, Aug 29, 2023, 7:19 PM +00:00

--- UCF/COP_3223C/t1.c
+++ UCF/COP_3223C/t1.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+// constant, data type, name, value
+const int YARDS_IN_MILE = 1760;
+
+#define FEET_IN_YARD 3
+
 int main(void) {
+    int feet_in_mile, num_miles;
     printf("hello, world\n");
 }
