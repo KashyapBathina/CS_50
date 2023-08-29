ffb4bdecabdd8a840d684ef63dddfe9f6c753c48
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-08-29 19:23:29 +0000

    Tue, Aug 29, 2023, 7:23 PM +00:00

--- UCF/COP_3223C/t1.c
+++ UCF/COP_3223C/t1.c
@@ -3,9 +3,16 @@
 // constant, data type, name, value
 const int YARDS_IN_MILE = 1760;
 
+// another way to define constant
 #define FEET_IN_YARD 3
 
 int main(void) {
     int feet_in_mile, num_miles;
-    printf("hello, world\n");
+    feet_in_mile = YARDS_IN_MILE * FEET_IN_YARD;
+
+    printf("How many miles did you run?\n");
+    scanf("%d", &num_miles)
+
+    printf("You ran a total of %d feet. \n", feet_in_mile*num_miles);
+    return 0;
 }
