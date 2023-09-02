157f3318ab0ee76e70bda4291fde4007ce267f1d
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-02 23:35:20 +0000

    Sat, Sep 2, 2023, 11:35 PM +00:00

--- UCF/COP_3223C/lecture_assignment1.c
+++ UCF/COP_3223C/lecture_assignment1.c
@@ -9,23 +9,23 @@ int main(void) {
 
     // reading in first input
     scanf("%d", &var1);
-    printf("You entered: %d", &var1);
+    printf("You entered: %d\n", var1);
 
     scanf("%d", &var2);
-    printf("You entered: %d", &var2);
+    printf("You entered: %d\n", var2);
 
     scanf("%d", &var3);
-    printf("You entered: %d", &var3);
+    printf("You entered: %d\n", var3);
 
     scanf("%d", &var4);
-    printf("You entered: %d", &var4);
+    printf("You entered: %d\n", var4);
 
     scanf("%d", &var5);
-    printf("You entered: %d", &var5);
+    printf("You entered: %d\n", var5);
 
     sum = var1 + var2 + var3 + var4 + var5;
     average = sum/5;
 
-    printf("The sum of the numbers you entered is: %d", &sum);
-    printf("The average is: %d", &average);
+    printf("The sum of the numbers you entered is: %d\n", sum);
+    printf("The average is: %f\n", average);
 }
