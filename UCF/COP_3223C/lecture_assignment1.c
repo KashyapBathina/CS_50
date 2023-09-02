e2695e4f6bf3f8190282698e4e266b788ee181d6
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-02 22:43:29 +0000

    Sat, Sep 2, 2023, 10:43 PM +00:00

---
+++ UCF/COP_3223C/lecture_assignment1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Program 1
+int main(void) {
+    // declaring variables, the sum, and the average
+    long int var1, var2, var3, var4, var5;
+    long int sum;
+    double average;
+
+    // reading in first input
+    scanf("%d", var1);
+    printf("You entered: %d", &var1);
+
+    scanf("%d", var2);
+    printf("You entered: %d", &var2);
+
+    scanf("%d", var3);
+    printf("You entered: %d", &var3);
+
+    scanf("%d", var4);
+    printf("You entered: %d", &var4);
+
+    scanf("%d", var5);
+    printf("You entered: %d", &var5);
+
+    sum = var1 + var2 + var3 + var4 + var5;
+    average = sum/5;
+
+    printf("The sum of the numbers you entered is: %d", &sum);
+    printf("The sum of the numbers you entered is: %d", &sum);
+}
