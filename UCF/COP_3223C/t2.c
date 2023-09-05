728b167fb0efeda5d7a0e380a99d939f91c0c537
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-05 19:18:07 +0000

    Tue, Sep 5, 2023, 7:18 PM +00:00

--- UCF/COP_3223C/t2.c
+++ UCF/COP_3223C/t2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+    Implicit Casting (automatically) - converting a smaller type to a larger type size. char -> int -> long -> float -> double.
+    Explicit Casting (manually) - converting a larger type to a smaller size type. double -> float -> long -> int -> char.
+*/
+
 int main(void) {
     printf("What is your name?\n");
     scanf()
