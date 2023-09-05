2ff899ff730b4974b8e8de31e01aa8812cb19b85
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-05 19:23:57 +0000

    Tue, Sep 5, 2023, 7:23 PM +00:00

--- UCF/COP_3223C/t2.c
+++ UCF/COP_3223C/t2.c
@@ -6,6 +6,26 @@
 */
 
 int main(void) {
-    printf("What is your name?\n");
-    scanf()
+    int grade1 = 75;
+    int grade2 = 80;
+
+    int average1 = (grade1+grade2)/2;
+    int average2 = grade1+grade2/2;
+    int average3 = grade2+grade1/2;
+    double average4 = (grade1+grade2)/2;
+    double average5 = (double)(grade1+grade2)/2;
+    double average6 = (grade1+grade2)/2.0;
+    double average7 = (double)((grade1+grade2)/2);
+    int average8 = (grade1+grade2/2.0);
+
+    //&d=placeholder
+    printf("Avg #1 = %d\n", average1);
+    printf("Avg #2 = %d\n", average2);
+    printf("Avg #3 = %d\n", average3);
+    printf("Avg #4 = %d\n", average4);
+    printf("Avg #5 = %d\n", average5);
+    printf("Avg #6 = %d\n", average6);
+    printf("Avg #7 = %d\n", average7);
+    printf("Avg #8 = %d\n", average8);
+
 }
