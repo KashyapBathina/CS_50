dc2f3c95e04742a0abcd019bd9a8f609cfd2c24c
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-07 19:11:01 +0000

    Thu, Sep 7, 2023, 7:11 PM +00:00

--- UCF/COP_3223C/t3.c
+++ UCF/COP_3223C/t3.c
@@ -3,18 +3,22 @@
 /*------------module 3------------------------------------------------------------------------------------------------------------/*
 
 /*
-Functions
+Functions requirements
     1. name of a function
     2. the data type of paramaters
     3. the return type of the function
 
-Printf, scanf
-Random number function-std library:
-    int x=1+rand()%100;
-C math library = #include <math.h>:
-    int=return type abs=function name, int=data type of paramater: int abs(int x)
-    double pow(double x, double y)
-    double sqrt(double x)
+Functions:
+    Printf, scanf
+    Random number function-std library:
+        int x=1+rand()%100;
+    C math library = #include <math.h>:
+        int=return type abs=function name, int=data type of paramater: int abs(int x)
+        double pow(double x, double y)
+        double sqrt(double x)
+
+Conditional expression-expression is true or false
+
 */
 
 int main(void) {
