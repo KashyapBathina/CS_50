301a1cbb3920e233466a080e1311c822d8fe0c5d
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-21 19:29:05 +0000

    Thu, Sep 21, 2023, 7:29 PM +00:00

--- UCF/COP_3223C/t4.c
+++ UCF/COP_3223C/t4.c
@@ -117,4 +117,14 @@ int main() {
     // init 3-d array; indexes start with 0;
     int arr[2][3][2] = { { {0,6},{1,7},{2,8}}
                         { {3,9},{4,10}, {5,11} } };
+
+    //printing values of a 3-d array
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<3;j++) {
+            for (int k=0;k<2;k++) {
+                //%i is placeholder for array; index
+                printf("Element at arr[%i][%i][%i]=%d\n", );
+            }
+        }
+    }
 }
