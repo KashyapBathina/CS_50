1ef84fa62d1db9bfb9181bc3545ca0688840eb52
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-21 19:34:55 +0000

    Thu, Sep 21, 2023, 7:34 PM +00:00

--- UCF/COP_3223C/t4.c
+++ UCF/COP_3223C/t4.c
@@ -113,9 +113,10 @@ int main() {
 }
 */
 
+/*
 int main() {
     // init 3-d array; indexes start with 0;
-    int arr[2][3][2] = { { {0,6},{1,7},{2,8} }
+    int arr[2][3][2] = { { {0,6},{1,7},{2,8} },
                         { {3,9},{4,10}, {5,11} } };
 
     //printing values of a 3-d array
@@ -129,4 +130,9 @@ int main() {
     }
 
     return 0;
+}
+*/
+
+int main() {
+    
 }
