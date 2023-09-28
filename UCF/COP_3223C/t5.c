632c7d7d076545dc5e45d5c5fc971e3cb8156dd2
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-09-28 02:48:30 +0000

    Thu, Sep 28, 2023, 2:48 AM +00:00

--- UCF/COP_3223C/t5.c
+++ UCF/COP_3223C/t5.c
@@ -4,6 +4,12 @@
 #include <time.h>
 
 int main() {
-    
-
+    int counter = 2;
+    while (counter < 10) {
+        counter ++;
+        if ( counter < 5 ) continue;
+        else if ( counter >=6 ) break;
+        printf("Inside Loop");
+    }
+    printf("Outside Loop\n");
 }
