b492f50b65171627e8bd1ced655d662d124402b8
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-03 19:10:05 +0000

    Tue, Oct 3, 2023, 7:10 PM +00:00

--- UCF/COP_3223C/t5.c
+++ UCF/COP_3223C/t5.c
@@ -8,7 +8,23 @@
 /*
 The ability to read and write large datatstes to files
     fprintf--
+        purpose:
+        structure: fprintf(file_ptr, control_string, other arguments)
+                    pointer - tells the compiler to referece or look ata specific memory location for value
+                    control string -
+                    other arguments - read or write
     fcanf--
+        purpose:
+        structure: fcanf(file_ptr, control_string, other arguments)
+                    pointer - tells the compiler to referece or look ata specific memory location for value
+                    control string -
+                    other arguments -
+
+Requirements:
+    1. Find the file we hae to use a pointer - FILE *ifp;
+    2. Name of the file with mode: 'r' for read 'w' for writing data
+    3. 
+
 */
 
 int main() {
