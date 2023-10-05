3f588e20bc21b9d1afdc79b45835c7288347ed64
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-05 00:24:22 +0000

    Thu, Oct 5, 2023, 12:24 AM +00:00

--- UCF/COP_3223C/lecture_assignment2p1.c
+++ UCF/COP_3223C/lecture_assignment2p1.c
@@ -11,13 +11,24 @@
 
 int main() {
     //
+    int, years, months, weeks, days, hours;
     double now;
     #define leap_years_since_1970 13
 
     //
     now = time(NULL);
 
-    
+    //
+    int sec_in_hours = 60*60;
+    int sec_in_days = sec_in_hours * 24;
+    int sec_in_weeks = sec_in_days * 7;
+    double sec_in_months = sec_in_days * 30.42;
+    int sec_in_years = 
+
+
+    years =
+
+
 
     return 0;
 }
