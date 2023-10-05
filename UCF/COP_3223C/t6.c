7096710c326f22241f0e78479bdab4b4913b9d25
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-05 19:11:25 +0000

    Thu, Oct 5, 2023, 7:11 PM +00:00

--- UCF/COP_3223C/t6.c
+++ UCF/COP_3223C/t6.c
@@ -6,7 +6,7 @@
 /*------------module 6------------------------------------------------------------------------------------------------------------*/
 
 /*
-Arra:data struc that holds series of variables with the same datatype
+Array:data struc that holds series of variables with the same datatype
     Gen Struct: type var name[size];
                 char colors[5];
                 start array with 0 and goes to 4
@@ -16,11 +16,10 @@ Arra:data struc that holds series of variables with the same datatype
     Common Mistakes:
                 1. Out of bounds: happends when your index into array is invalid when yu try to reference it
                 2. Tyring to assign an entire array a valus such as test_scores=7;
-
-
+                3. Not recognizing the differnece between an array index and values stored in a particular index
 */
 
 int main() {
-
+    int 
 
 }
