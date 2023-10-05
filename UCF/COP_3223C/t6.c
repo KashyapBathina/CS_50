5f0810984771f58e8de2b9e84a38f59c32f3bb1a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-05 19:06:52 +0000

    Thu, Oct 5, 2023, 7:06 PM +00:00

--- UCF/COP_3223C/t6.c
+++ UCF/COP_3223C/t6.c
@@ -10,6 +10,9 @@ Arra:data struc that holds series of variables with the same datatype
     Gen Struct: type var name[size];
                 char colors[5];
                 start array with 0 and goes to 4
+                int numbers[10];
+                numbers[0] = 1;
+                numbers[1] = 2;
 
 */
 
