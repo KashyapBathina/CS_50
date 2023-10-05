4e11e0927805efd3e43b1c49d11e59825d7871af
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-05 02:21:10 +0000

    Thu, Oct 5, 2023, 2:21 AM +00:00

--- UCF/COP_3223C/lecture_assignment2p1.c
+++ UCF/COP_3223C/lecture_assignment2p1.c
@@ -12,7 +12,7 @@
 int main() {
     //
     int years, months, weeks, days, hours;
-    double now;
+    int now;
     #define leap_years_since_1970 13
 
     //
@@ -26,13 +26,13 @@ int main() {
     int sec_in_years = sec_in_days * 365;
 
     years = now / sec_in_years;
-    int t1 = (now % sec_in_years);
+    int t1 = now % sec_in_years;
     months = t1/sec_in_months;
-    int t2 = (t1 % sec_in_months);
+    int t2 = t1 % sec_in_months;
     weeks = t2/sec_in_weeks;
-    int t3 = (t2 % sec_in_weeks);
+    int t3 = t2 % sec_in_weeks;
     days = t3/sec_in_days;
-    int t4 = (t3 % sec_in_days);
+    int t4 = t3 % sec_in_days;
     hours = t4/sec_in_hours;
 
     printf("%d", now);
