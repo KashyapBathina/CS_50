88524e6e61e009e6fe26d66396a79dd98ac0598a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-05 19:05:13 +0000

    Thu, Oct 5, 2023, 7:05 PM +00:00

---
+++ UCF/COP_3223C/t6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/*------------module 6------------------------------------------------------------------------------------------------------------*/
+
+/*
+Arra:data struc that holds series of variables with the same datatype
+    Gen Struct: type var name[size];
+                char colors[5];
+                start array with 0 and goes to 4
+
+*/
+
+int main() {
+
+
+}
