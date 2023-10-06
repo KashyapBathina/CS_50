01fb06e7ec32080fcc922841cff2c9ea64de5097
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-06 16:46:34 +0000

    Fri, Oct 6, 2023, 4:46 PM +00:00

--- UCF/COP_3223C/lecture_assignment2p1.c
+++ UCF/COP_3223C/lecture_assignment2p1.c
@@ -11,7 +11,7 @@
 
 int main() {
     //
-    int years, months, weeks, days, hours;
+    int years, months, weeks, days, hours, minutes, seconds;
     int now;
     #define leap_years_since_1970 13
 
@@ -19,25 +19,35 @@ int main() {
     now = time(NULL);
 
     //
+    int sec_in_minutes = 60;
     int sec_in_hours = 60*60;
     int sec_in_days = sec_in_hours * 24;
     int sec_in_weeks = sec_in_days * 7;
     int sec_in_months = sec_in_days * 30.42;
     int sec_in_years = sec_in_days * 365;
 
+    // *NOTE*: I was told by the professor to add the seconds accumulated from leap years
+    // Since there are 13 leap years, there are 13 extra days; we can add the seconds from those days
+    now += 13*sec_in_days;
+
     //
     years = now / sec_in_years;
     int t1 = now % sec_in_years;
-    months = t1/sec_in_months;
+    months = t1 / sec_in_months;
     int t2 = t1 % sec_in_months;
-    weeks = t2/sec_in_weeks;
+    weeks = t2 / sec_in_weeks;
     int t3 = t2 % sec_in_weeks;
-    days = t3/sec_in_days;
+    days = t3 / sec_in_days;
     int t4 = t3 % sec_in_days;
-    hours = t4/sec_in_hours;
+    hours = t4 / sec_in_hours;
+    int t5 = t4 % sec_in_hours;
+    minutes = t5 / sec_in_minutes;
+    int t6 = t5 % sec_in_minutes;
+    seconds = t6;
 
+    // *NOTE*: I was also told by the professor to also ouput the minutes and seconds
     //
-    printf("Time passed since Jan 1, 1970: %d years, %d months, %d weeks, %d days, and %d hours\n", years, months, weeks, days, hours);
+    printf("Time passed since Jan 1, 1970: %d years, %d months, %d weeks, %d days, %d hours, %d minutes, and %d seconds\n", years, months, weeks, days, hours, minutes, seconds);
 
     return 0;
 }
