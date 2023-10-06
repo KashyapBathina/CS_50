f329a8dc7444763c692fb80d2f5dc9e15433a933
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-06 17:46:59 +0000

    Fri, Oct 6, 2023, 5:46 PM +00:00

--- UCF/COP_3223C/lecture_assignment2p1.c
+++ UCF/COP_3223C/lecture_assignment2p1.c
@@ -12,6 +12,8 @@
     years, months, etc.) and then using modulus to get the remaining seconds
     and repeat for all time variables. The program also adds the seconds accum-
     alated from leap years.
+    note: PLEASE understand that I included leap years because the professor told me
+    to when I asked him
 */
 
 
@@ -34,7 +36,7 @@ int main() {
 
     // *NOTE*: I was told by the professor to add the seconds accumulated from leap years
     // since there are 13 leap years, there are 13 extra days; we can add the seconds from those days
-    // now += 13*sec_in_days;
+    now += 13*sec_in_days;
 
     // finding out the years, months, etc. by dividing to get a quotient and using the remaining with modulus
     // and then repeating for every time types
