421b0ff6cd1389c453f1ab80197809d75a621b18
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-06 17:22:34 +0000

    Fri, Oct 6, 2023, 5:22 PM +00:00

--- UCF/COP_3223C/lecture_assignment2p1.c
+++ UCF/COP_3223C/lecture_assignment2p1.c
@@ -8,19 +8,22 @@
     date: 9/3/23
     purpose: This program outputs the years, months, weeks,
     days, hours, minutes, and seconds passed since Jan 1, 1970;
-    it does this by using dividing to get a 
+    it does this by using dividing to get a quotient (represeting the
+    years, months, etc.) and then using modulus to get the remaining seconds
+    and repeat for all time variables. The program also adds the seconds accum-
+    alated from leap years.
 */
 
 int main() {
-    //
+    // declaring variables and the constant of the leap years since 1970;
     int years, months, weeks, days, hours, minutes, seconds;
     int now;
     #define leap_years_since_1970 13
 
-    //
+    // getting the number of seconds since Jan 1, 1970
     now = time(NULL);
 
-    //
+    // using arithmetic to find the number of seconds in various time elements
     int sec_in_minutes = 60;
     int sec_in_hours = 60*60;
     int sec_in_days = sec_in_hours * 24;
@@ -29,10 +32,11 @@ int main() {
     int sec_in_years = sec_in_days * 365;
 
     // *NOTE*: I was told by the professor to add the seconds accumulated from leap years
-    // Since there are 13 leap years, there are 13 extra days; we can add the seconds from those days
+    // since there are 13 leap years, there are 13 extra days; we can add the seconds from those days
     //now += 13*sec_in_days;
 
-    //
+    // finding out the years, months, etc. by dividing to get a quotient and using the remaining with modulus
+    // and then repeating for every time types
     years = now / sec_in_years;
     int t1 = now % sec_in_years;
     months = t1 / sec_in_months;
@@ -48,7 +52,7 @@ int main() {
     seconds = t6;
 
     // *NOTE*: I was also told by the professor to also ouput the minutes and seconds
-    //
+    // printing out the years, months, weeks, days, hours, minutes, and seconds that passed since Jan 1, 1970
     printf("Time passed since Jan 1, 1970: %d years, %d months, %d weeks, %d days, %d hours, %d minutes, and %d seconds\n", years, months, weeks, days, hours, minutes, seconds);
 
     return 0;
