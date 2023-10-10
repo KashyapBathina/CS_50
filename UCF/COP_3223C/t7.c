a6b44b73d1aa004662460774a4fb3d6cc54acdb7
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-10 19:05:32 +0000

    Tue, Oct 10, 2023, 7:05 PM +00:00

---
+++ UCF/COP_3223C/t7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/*------------module 7------------------------------------------------------------------------------------------------------------*/
+
+/*
+2-d Arrays:
+    Gen struct: type var[y][x]; where y is row and x is column
+                var[0][0] = 2;
+
+
+*/
+
+int main() {
+
+}
