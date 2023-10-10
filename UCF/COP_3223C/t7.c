32b675c80b9f2584c5a3c6e2db12c8ffacc7a8dd
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-10 19:26:11 +0000

    Tue, Oct 10, 2023, 7:26 PM +00:00

--- UCF/COP_3223C/t7.c
+++ UCF/COP_3223C/t7.c
@@ -9,6 +9,8 @@
 2-d Arrays:
     Gen struct: type var[y][x]; where y is row and x is column
                 var[0][0] = 2;
+
+Pointers: store memory addresses of other variables
 */
 
 int main() {
