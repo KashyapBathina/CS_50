1f154de8961e81294024741e10e77b516f3cc7d7
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-19 18:26:33 +0000

    Thu, Oct 19, 2023, 6:26 PM +00:00

--- UCF/COP_3223C/lecture_assignment3p3.c
+++ UCF/COP_3223C/lecture_assignment3p3.c
@@ -6,23 +6,26 @@
 /*
     name: Kashyap Bathina
     date: 10/18/23
-    purpose: This program 
+    purpose: This program declares and array of 100 ints and assigns them random
+    values using a ran() funct and then totals them and divides by 100 to find average.
 */
 
 int main(void) {
-    //
-    int index, average, total=0, arr[100];
+    // declaring vars and array of size 100
+    int index, total=0, arr[100];
+    // double average;
 
     // seeding the rand number to prevent repeated occurences
     srand(time(NULL));
 
-    //
+    // making a for loop that iterates over the array and assigns the indexes random numbers
+    // and then adds that number to the total which starts at 0
     for (index = 0; index < 100; index++) {
         arr[index] = rand() % 100 + 1;
         total += arr[index];
     }
 
-    //
+    // finds and prints the average of the elements in the array by dividing total by 100 (total num)
     average = (double) total / 100;
     printf("Average of 100 Random Numbers: %d\n", average);
 
