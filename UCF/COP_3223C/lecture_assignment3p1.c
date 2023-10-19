04c9253f960b029788563c68dccd704df69a4c34
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-19 00:45:23 +0000

    Thu, Oct 19, 2023, 12:45 AM +00:00

---
+++ UCF/COP_3223C/lecture_assignment3p1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+    name: Kashyap Bathina
+    date: 9/3/23
+    purpose: This program takes in five values from the user and then
+    outputs the sum, by adding all the numbers and the average, by dividing
+    the sum by the number of values--5.
+*/
+
+int main(void) {
