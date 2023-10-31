76e7e29928209f007fd7a0928cb4cace212d7320
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-10-31 19:10:36 +0000

    Tue, Oct 31, 2023, 7:10 PM +00:00

--- UCF/COP_3223C/t8.c
+++ UCF/COP_3223C/t8.c
@@ -10,6 +10,10 @@
 
 void printArray(int vals[], int size);
 void fillArray(int vals[], int size, int max);
+void swap(int vals[], int i, int j);
+void move(int vals[], int maxIndex);
+void simpleSort(int vals[], int size);
+void bubbleSort(int vals[], int size);
 
 int main() {
 
