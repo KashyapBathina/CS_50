c3fa3cbb68973440d8654805d94b6a13bd23a77f
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-11-07 20:05:21 +0000

    Tue, Nov 7, 2023, 8:05 PM +00:00

---
+++ UCF/COP_3223C/t9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+/*------------module 8------------------------------------------------------------------------------------------------------------*/
+
+/*
+    Struct: array that allows you to store related variables of the same type in same struct
+        Gen struct: struct <struct> {
+                
+        }
