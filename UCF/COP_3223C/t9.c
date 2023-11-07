19bfa24f15ac1cd1a95c335355b8f6e65fe46778
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-11-07 20:09:57 +0000

    Tue, Nov 7, 2023, 8:09 PM +00:00

--- UCF/COP_3223C/t9.c
+++ UCF/COP_3223C/t9.c
@@ -8,11 +8,19 @@
 
 /*
     Struct: array that allows you to store related variables of the same type in same struct, usually put a declaration after #defines
-        Gen struct: struct <struct> {
+        Gen struct: struct name {
                 dataType var1;
                 dataType var2;
             }
 */
+
+struct block {
+    int num;
+    char letter;
+    char color[15];
+}
+
+void setup(struct block *);
 
 int main() {
 
