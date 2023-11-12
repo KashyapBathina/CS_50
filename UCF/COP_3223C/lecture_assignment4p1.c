b9d7d50697cf2a94a5bf685979f1c525d953ccc7
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-11-12 01:00:57 +0000

    Sun, Nov 12, 2023, 1:00 AM +00:00

--- UCF/COP_3223C/lecture_assignment4p1.c
+++ UCF/COP_3223C/lecture_assignment4p1.c
@@ -6,17 +6,20 @@
 /*
     name: Kashyap Bathina
     date: 11/6/23
-    purpose: This programs lorem ipsum
+    purpose: This programs takes in an array of size n, which is chosen by the user, and
+    then passes in the array and the size of the said array into a function--average. This
+    function does the arithmetic for the average by totaling the values and dividing by the
+    size, and returns that double back to main to be outputted.
 */
 
-//
+// declaring our function
 double average(int arr[], int size);
 
 int main(void) {
-    //
+    // declaring our variables, will declare array after we know size
     int arraySize;
 
-    //
+    // asking user for size of array and declaring array of said size
     printf("How many elements in array: ");
     scanf("%d", &arraySize);
     int array[arraySize];
