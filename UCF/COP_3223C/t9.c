2f9803f2f92c82ee7cd7e2dbe69da5a03c7bd3f0
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-11-14 23:23:39 +0000

    Tue, Nov 14, 2023, 11:23 PM +00:00

--- UCF/COP_3223C/t9.c
+++ UCF/COP_3223C/t9.c
@@ -22,6 +22,12 @@
             5. Important to concepts of structs and linked lists
         Malloc: used to dynamically assign a single block of memory with a certain size
             Gen Struct: ptr = (cast-type*) malloc(size of bytes);
+        Calloc: when we need continuous allocatin for a specified # of blocks memory for spec type
+            Gen Struct: prt = (cast-type*) calloc(n, element size); where n is element size and ES is size of each.
+        Free: used to dynamically deallocate memory, or give back borrowed.
+            Gen Struct: free(ptr);
+        Realloc: used to add or append more memory to whhat is already set aside;
+            Gen Struct: ptr = realloc(ptr, newSize); where ptr is reallocated with new size 'newSize'
 */
 
 /*
@@ -77,6 +83,7 @@ int equal(struct block a, struct block b) {
 }
 */
 
+/*
 void fun() {
     int a;
 }
@@ -85,5 +92,46 @@ int main() {
     int b;
     int c[10];
 }
+*/
 
+/*
+int main()
+{
+    // This pointer will hold the
+    // base address of the block created
+    int* ptr;
+    int n, i;
+    // Get the number of elements for the array
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Entered number of elements: %d\n", n);
+    // Dynamically allocate memory using malloc()
+    ptr = (int*)malloc(n * sizeof(int));
+    // Check if the memory has been successfully
+    // allocated by malloc or not
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    else {
+        // Memory has been successfully allocated
+        printf("Memory successfully allocated using malloc.\n");
+        // Get the elements of the array
+        for (i = 0; i < n; ++i) {
+            ptr[i] = i + 1;
+        }
+        // Print the elements of the array
+        printf("The elements of the array are: ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
+    }
+    return 0;
+}
+*/
+
+/*
+For Memory Allocation, Malloc, Calloc, and Free Code/Explanation, use:
+https://www.geeksforgeeks.org/dynamic-memory-allocation-in-c-using-malloc-calloc-free-and-realloc/#
+*/
 
