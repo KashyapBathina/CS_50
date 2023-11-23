f08d655de241d9a84720754148d6f84834f19ed2
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2023-11-23 19:21:22 +0000

    Thu, Nov 23, 2023, 7:21 PM +00:00

--- UCF/COP_3223C/lecture_assignment5.c
+++ UCF/COP_3223C/lecture_assignment5.c
@@ -4,3 +4,22 @@
 #include <math.h>
 #include <string.h>
 
+/*
+    name: Kashyap Bathina
+    date: 11/6/23
+    purpose: This programs takes in three integers and passes them through
+    a function which returns the largest of the three using if statements.
+    Then returns that number to main and prints it.
+*/
+
+// declaring functions
+void analyzeHand(int a, int b, int c);
+int largest_int(int a, int b, int c);
+int largest_int(int a, int b, int c);
+int largest_int(int a, int b, int c);
+
+
+// main function
+int main (void) {
+
+}
