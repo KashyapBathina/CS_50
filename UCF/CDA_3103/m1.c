dc53ce50a523b82e00419db5178b2a1b967c629a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-17 00:28:34 +0000

    Wed, Jan 17, 2024, 12:28 AM +00:00

--- UCF/CDA_3103/m1.c
+++ UCF/CDA_3103/m1.c
@@ -1,4 +1,5 @@
 /*
 Module 1:
-
+    Decimal, base 10, notation to represent integers: 965 = 9*10^2 + 6*10^1 + 5*10^0
+        n = a_kb^k
 */
---
+++ UCF/COP_3502/a.c
@@ -0,0 +1,4 @@
+/*
+    bit-single 0 or 1,  bytye-8 consec digits,  word-4 bytes
+
+*/
