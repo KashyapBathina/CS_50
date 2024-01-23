f8b76af45558f5235c9f95eda3bfba280d14d3f5
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-23 23:40:34 +0000

    Tue, Jan 23, 2024, 11:40 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -1,7 +1,7 @@
 /*
 stack vs heap:
-    stack: allocation happens on contiguous blocks of memory (array); temporary memory allocation since de-allocation after method
-    heap:
+    stack: allocation happens on contiguous blocks of memory (array); temporary memory allocation since de-allocation after method; data only added and deleted from top
+    heap: allocated during the execution and pile of memory space available to programmers to allocate and de-allocate; data stored and removed in any order
 
     bit-single 0 or 1,  bytye-8 consec digits,  word-4 bytes
 
