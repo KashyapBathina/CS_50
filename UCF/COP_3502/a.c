12705ca6f324cf7367563a946d044ae4fd87e0f4
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-23 23:52:08 +0000

    Tue, Jan 23, 2024, 11:52 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -4,8 +4,9 @@ stack vs heap:
     heap: allocated during the execution and pile of memory space available to programmers to allocate and de-allocate; data stored and removed in any order; variables can be resized
 
 pointers:
-    int *p,q,r--p integer
+    int *p, q, r--p integer
     int* p, q, r--
+    int *p, *q, *r--all
 
 basics:
     bit-single 0 or 1,  bytye-8 consec digits,  word-4 bytes
