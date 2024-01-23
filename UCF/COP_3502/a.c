12a6fe74e1668338fe5bfba55157fd2155b8c1ad
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-23 23:35:45 +0000

    Tue, Jan 23, 2024, 11:35 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -1,6 +1,6 @@
 /*
     stack vs heap:
-        stack: 
+        stack: allocation happens on contiguous blocks of memory (array); temporary memory allocation since de-allocation after 
     bit-single 0 or 1,  bytye-8 consec digits,  word-4 bytes
 
 */
