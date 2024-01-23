d10c84a55c9a14b2e2401a62fbb7c68687ed5152
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-23 23:44:32 +0000

    Tue, Jan 23, 2024, 11:44 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -1,8 +1,12 @@
 /*
 stack vs heap:
     stack: allocation happens on contiguous blocks of memory (array); temporary memory allocation since de-allocation after method; data only added and deleted from top
-    heap: allocated during the execution and pile of memory space available to programmers to allocate and de-allocate; data stored and removed in any order
+    heap: allocated during the execution and pile of memory space available to programmers to allocate and de-allocate; data stored and removed in any order; variables can be resized
 
+pointers:
+    int *p,q,r-
+
+basics:
     bit-single 0 or 1,  bytye-8 consec digits,  word-4 bytes
 
 */
