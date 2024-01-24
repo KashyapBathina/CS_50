415bd2ed7739ed07635b48a9f914a8406d335a7b
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-24 22:50:35 +0000

    Wed, Jan 24, 2024, 10:50 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -87,6 +87,7 @@ file i/o:
         when "a+": append or create file for r/w and
     reading input: ch=fgetc(fp) reads char from current pointer position & advances pointer position so that it now points to the next character
     and returns character that is read which we collect to the variable ch
+    closing: int fclose(FILE *fp) closes stream, writes any data remaining in disk buffer to file and frees, returns 0 if success
 */
 
 
