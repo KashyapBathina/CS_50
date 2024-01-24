076ca565f7bdf00ddd798792e92019402a06b0d4
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-24 22:56:28 +0000

    Wed, Jan 24, 2024, 10:56 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -88,7 +88,7 @@ file i/o:
     reading input:
         fgetc: ch=fgetc(fp) reads char from current pointer position & advances pointer position so that it now points to the next character
         and returns character that is read which we collect to the variable ch; feof() checks whether EOF;
-        fgets: fgets  (str, 100,  ifp)
+        fgets: fgets(str, 100,  ifp) reads data one line at a time 
 
     closing: int fclose(FILE *fp) closes stream, writes any data remaining in disk buffer to file and frees, returns 0 if success;
 */
