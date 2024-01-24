69b1da34c3ff43efa07c7e0049d518e1be5f25c7
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-24 23:14:25 +0000

    Wed, Jan 24, 2024, 11:14 PM +00:00

--- UCF/COP_3502/a.c
+++ UCF/COP_3502/a.c
@@ -91,7 +91,7 @@ file i/o:
         fgets: char *fgets(char *str, int n, FILE *stream) reads data one line at a time until null termination; str is pointer to array of char,
         n is max # characters to be read including null, stream is pointer to file; fgets keeps \n in str and you can remove withstr[strcspn(str, "\n")] = 0
     writing output:
-        fscanf & fprintf
+        fscanf & fprintf: noting most files have 1 integer at top that reps # cases
 
     closing: int fclose(FILE *fp) closes stream, writes any data remaining in disk buffer to file and frees, returns 0 if success;
 */
