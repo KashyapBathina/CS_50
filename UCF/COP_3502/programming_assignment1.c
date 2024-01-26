d09b0da7080dafbe3e551de23c061ba08e4c00d6
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-26 05:46:34 +0000

    Fri, Jan 26, 2024, 5:46 AM +00:00

--- UCF/COP_3502/programming_assignment1.c
+++ UCF/COP_3502/programming_assignment1.c
@@ -4,9 +4,15 @@
 
 
 int main () {
-    char string1 = (char*) malloc(100002 * sizeof(char))
-    char string2 = (char*) malloc(100002 * sizeof(char))
+    char* string1 = (char*) malloc(100002 * sizeof(char));
+    char* string2 = (char*) malloc(100002 * sizeof(char));
 
-    fgets(string1,20,stdin);
+    fgets(string1, 100002, stdin);
+    fgets(string2, 100002, stdin);
+
+    printf("%s", string1);
+    printf("%s", string2);
+
+
     return 0;
 }
