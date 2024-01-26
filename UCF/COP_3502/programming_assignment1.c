e7ec9fee3846a1462d319faa84df64ef68cd7bf9
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-26 06:05:12 +0000

    Fri, Jan 26, 2024, 6:05 AM +00:00

--- UCF/COP_3502/programming_assignment1.c
+++ UCF/COP_3502/programming_assignment1.c
@@ -4,8 +4,7 @@
 
 
 int main () {
-    int freq1[26];
-    int freq2[26];
+    int freq1[26], freq2[26], letters_required = 0;
     char* string1 = (char*) malloc(100002 * sizeof(char));
     char* string2 = (char*) malloc(100002 * sizeof(char));
 
@@ -15,7 +14,19 @@ int main () {
     printf("%s", string1);
     printf("%s", string2);
 
-    for (int i=0; i<strlen(string1))
+    for (int i=0; i<strlen(string1)-1;i++) {
+        freq1[string1[i]-'A']++;
+    }
+
+    for (int i=0; i<strlen(string1)-1;i++) {
+        freq1[string2[i]-'A']++;
+    }
+
+    for (int i=0; i<26; i++) {
+        letters_required += freq2[i] - freq1[i];
+    }
+
+    printf("%d", letters_required);
 
     return 0;
 }
