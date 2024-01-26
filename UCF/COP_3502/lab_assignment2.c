3800317aa810f6b9440c482c1f83506c2958eb42
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-26 16:32:20 +0000

    Fri, Jan 26, 2024, 4:32 PM +00:00

--- UCF/COP_3502/lab_assignment2.c
+++ UCF/COP_3502/lab_assignment2.c
@@ -11,10 +11,10 @@ struct Node{
 // This function updates a singly linked list into a circular singly linked list
 struct Node* makeListCircular(struct Node* pHead){
     struct Node* pTemp = pHead;
-    while (pTemp->mNext != null) {
+    while (pTemp->mNext != NULL) {
         pTemp = pTemp->mNext;
     }
-    pTemp->next = pHead;
+    pTemp->mNext = pHead;
 }
 
 
