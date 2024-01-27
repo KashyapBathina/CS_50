c41a6db48a33da12e3081b922e10c0d6b6c82cee
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-01-27 22:25:36 +0000

    Sat, Jan 27, 2024, 10:25 PM +00:00

--- UCF/COP_3502/programming_assignment1.c
+++ UCF/COP_3502/programming_assignment1.c
@@ -6,23 +6,27 @@
     name: Kashyap Bathina
     date: 1/25/24\
     purpose: after taking in two strings, the program calculates how many new letters are needed for the second
-    message, subtracting the letters from the 1st message. with a frequency array of both strings noting how many
-    of each alphabet there are, the program subtracts 
+    message, subtracting the letters from the 1st message. after reading each character of the array through a
+    frequency array of both strings noting how many of each alphabet there are, the program subtracts string 2's
+    array from string 1's array when the # of characters are greater than string 1's at that index. totaling this
+    number gets the # of new letters needed for the new sign.
 */
 
+// declaring the constannts of alphabets and the max size of the strings
 const int LTTRS_IN_ALPHABET = 26;
 const int STRING_SIZE = 100002;
 
 int main () {
+    // declaring variables: initializing freq arrays and letters required to zero
     int freq1[LTTRS_IN_ALPHABET] = {0}, freq2[LTTRS_IN_ALPHABET] = {0}, letters_required = 0;
+
+    // dynamically allocating memory for two strings of STRING max SIZE via malloc
     char* string1 = (char*) malloc(STRING_SIZE * sizeof(char));
     char* string2 = (char*) malloc(STRING_SIZE * sizeof(char));
 
+    // using fgets on the stdin to read in user input and storing it in the previously allocated char array
     fgets(string1, 100002, stdin);
     fgets(string2, 100002, stdin);
-
-    printf("%s", string1);
-    printf("%s", string2);
 
     for (int i=0; i<strlen(string1)-1;i++) {
         if (string1[i] != '\n' && string1[i] >= 'A' && string1[i] <= 'Z') {
