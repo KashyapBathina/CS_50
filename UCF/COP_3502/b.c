7ad66081a2b330ae981acfe7e5039d55afe1f63b
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-01 05:07:31 +0000

    Thu, Feb 1, 2024, 5:07 AM +00:00

--- UCF/COP_3502/b.c
+++ UCF/COP_3502/b.c
@@ -6,7 +6,14 @@ stack:
     size limited and can crash if too large; memory auto destroyed/freed after funct
 
 dynamic:
-    memory space ! known at compile time
+    memory space ! known at compile time; no name of allocated memory, instead pointer to adress; memory ! freed auto; takes space from heap memory, ! stack
+    malloc:
+        void *malloc(size_t size) allocates unused space; space contains garbadge vals; returns a pointer to the beginning of the memory allocated; if memory ! found, null returned
+        syntax: int *p = (int *) malloc(sizeof(int)); casting null pointer to int pointer that points to that address
+    calloc:
+        
+
+
 */
 
 
