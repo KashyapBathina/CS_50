16cbe77fe423bf1d6f2565183d3760dfca086d3e
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-01 05:18:49 +0000

    Thu, Feb 1, 2024, 5:18 AM +00:00

--- UCF/COP_3502/b.c
+++ UCF/COP_3502/b.c
@@ -10,6 +10,8 @@ dynamic:
     malloc: void *malloc(size_t size) allocates unused space; space contains garbadge vals; returns a pointer to the beginning of the memory allocated; if memory ! found, null returned
         syntax: int *p = (int *) malloc(sizeof(int)); casting null pointer to int pointer with (int *), b/c malloc returns null pointer, that points to that address
     calloc: void *calloc(size_t n, size_t e); allocates array of size n with each element of size e; unlike malloc, each space initialized to 0; int *p = (int *) calloc(1, sizeof(int));
+    memory leak: int *p1 = malloc . . .; int *p2 = malloc . . .; for(loop) {scan into &p1[i]} p2 = p1; return p1: memory leak, b/c after p2=p1, both pointers are pointing to p2; allocated
+        memory for p2 got disconnected since we don't know adress anymore and still alive, but no longer able to freed and just takes space
 
 
 */
