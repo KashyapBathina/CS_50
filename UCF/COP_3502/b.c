83e8d603b633a502261b5427969501608b8dfe2a
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-01 04:38:10 +0000

    Thu, Feb 1, 2024, 4:38 AM +00:00

--- UCF/COP_3502/b.c
+++ UCF/COP_3502/b.c
@@ -1,6 +1,9 @@
 /*
 stack:
-    allocated space not moving, memory requirements are known at compile time,
+    allocated space not moving, memory requirements are known at compile time; after compiles, can perfectly predict how much memory will be needed; space used whether input taken
+    memory only reserved while function is running; declaring a variable and array is statically located: cannot return array after funct b/c array deleted after funct
+
+x
 */
 
 
