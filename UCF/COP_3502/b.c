5da7668987bd5ce314c2aac634d79904a2818b12
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-01 05:02:06 +0000

    Thu, Feb 1, 2024, 5:02 AM +00:00

--- UCF/COP_3502/b.c
+++ UCF/COP_3502/b.c
@@ -2,10 +2,11 @@
 stack:
     allocated space not moving, memory requirements are known at compile time; after compiles, can perfectly predict how much memory will be needed; space used whether input taken
     memory only reserved while function is running; declaring a variable and array is statically located: cannot return array after funct b/c array deleted after funct
-    code:int size; scanf(“%d”, &size); nt Arr[size]; size of arr ! known bfore compile: therfore uses a memory pool called stack to allocate this memory, high chance of crash b/c stack
-    size limited and can crash if too large
+    code:int size; scanf(“%d”, &size); nt Arr[size]; size of arr ! known bfore compile: therfor uses a memory pool called stack to allocate this memory, high chance of crash b/c stack
+    size limited and can crash if too large; memory auto destroyed/freed after funct
 
-x
+dynamic:
+    memory space ! known at compile time
 */
 
 
