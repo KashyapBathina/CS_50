cd3f80118a5fe9244e874545309be6c25f4eb577
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-12 04:46:01 +0000

    Mon, Feb 12, 2024, 4:46 AM +00:00

--- c.c
+++ c.c
@@ -1,4 +1,5 @@
 /*
 stack:
-    abstract data type; last in first out; anything added goes on top; anything removed must be on top; ie stack of plates; basic operations: 
+    abstract data type; last in first out; anything added goes on top; anything removed must be on top; ie stack of plates; basic operations: push, pop, isempty, isfull, peek
+    push: when maxSize = 6; stack[maxSize]; top=-1 -> push(char x) = stack[++top] = x (remeber )
 */
