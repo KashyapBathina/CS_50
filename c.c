e5eccff4463e3b56aa5dac519cd08e4fedda48cf
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-12 04:56:19 +0000

    Mon, Feb 12, 2024, 4:56 AM +00:00

--- c.c
+++ c.c
@@ -3,7 +3,7 @@ stack:
     abstract data type; last in first out; anything added goes on top; anything removed must be on top; ie stack of plates; basic operations: push, pop, isempty, isfull, peek
     push: when maxSize = 6; stack[maxSize]; top=-1 -> push(char x) {stack[++top] = x}(remember ++i increments first (why ++ front) and returns incremented val; also ++top
         inside [] still icrements top and is the same as ++top; stack[top]); what push does is set increase index and set index = x
-    pop: return stack[top--]; returns value before -- and sets top--, so
+    pop: return stack[top--]; returns value before -- and sets top--; note doesn't remove element at past top, just changes refrnce point so can be overwritten by push
     peek: return stack[top]; returns top item in the stack
 
 
