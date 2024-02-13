8c2e067d88b9be0fae1c5ad651e77e85638b635e
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-13 06:15:10 +0000

    Tue, Feb 13, 2024, 6:15 AM +00:00

--- UCF/COP_3502/c.c
+++ UCF/COP_3502/c.c
@@ -48,6 +48,7 @@ queues:
             noe=num of elmnts, size=size of queue then increase noe++; do this if noe<size (if there is capacity), in this case, can stop, or make more memory
             realloc: b/c challenges using (f+noe) % size formula, solution-after realloc, copy all data before final, to index starting from old size and afterwards, then
             finally add item to next slot-> then can use spaces in front again with (f+noe) % size formula
+            summary: 1. get item wanna insert 2. check if que full by comparing noe and size
 
 */
 
