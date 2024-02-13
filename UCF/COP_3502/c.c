3b067441aed92f957fa2141f4daf9a83a0db9023
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-13 04:36:24 +0000

    Tue, Feb 13, 2024, 4:36 AM +00:00

--- UCF/COP_3502/c.c
+++ UCF/COP_3502/c.c
@@ -18,13 +18,18 @@ stack:
             when reach end, check if stack empty:
         infix/postfix expression evaluation:
             infix--a + b *c or (a + b) * c - d * e; postfix--operators after operand: a + b -> ab+ && a + b * c = abc*+; operators are always in correct evaluation order
-            can use stack to express and evaluate; solution=1. read infix left to right -> 2. if operator (ie +, b/c operand ! go in stack), append to postfix string -> 3.
+            can use stack to express and evaluate; solution=1. read infix left to right -> 2. if operand (ie +, b/c operand ! go in stack), append to postfix string -> 3.
             comparing current operator read in vs operator on top of stack -> if current priority higher, current pushed to top stack; -> if same or lower, popping operators
             from stack until priority current higher than operator at top of stack -> popped appended to postfix in order popped -> current pushed to stack; if closing parenth,
             pop until open parenth found in stack, then append popped items to postfix expect parenthesis; priority: 1. power 2. /*% 3. +-
             ex: 4 * 2 + 3 + 8/4: solution:
-                1. postfix = 4          stack: empty
-                2. postfix = 4          stack:  
+                1. postfix = 4              stack: empty
+                2. postfix = 4              stack: *
+                3. postfix = 4 2            stack: *
+                4. postfix = 4 2 *          stack: +, current is +, since current < top, pop until priority current higher and append popped to postfix
+                5. postfix = 4 2 * 3        stack: +
+                6. postfix = 4 2 * 3 +      stack: +, current is next +, since current same as top (+), pop until priority current higher and append + to post
+                7. postfix = 4 2 * 3 + 8    stack: *
 
 
 */
