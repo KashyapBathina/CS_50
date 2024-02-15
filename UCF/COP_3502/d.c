af71ea3039284257fa8db2b6dc3c69df2062af73
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-15 07:18:12 +0000

    Thu, Feb 15, 2024, 7:18 AM +00:00

--- UCF/COP_3502/d.c
+++ UCF/COP_3502/d.c
@@ -10,5 +10,32 @@ recursion:
     general struct: 1. if (terminating condition), do final action; else, take 1 step closer to terminating condition by calling recursively on a smaller subproblem
         2. if (!(terminating condition)), take 1 step . . .
     example: decimal to binary: void dectobin(int n) {if (n < 2)  printf("%d", n);    else {dectobin(n/2); printf("%d", n%2);}}
-
+    example: fast exponent--recursive exponent becomes slow w large nums; so exploit law of exponents: 2^8 instead of = 2*2*2 . . . 8 times, 2^8 = 2^4 * 2^4, 2^4 = 2^2 * 2^2, 2^2
+        = 2 * 2; 3 mult better than 7; hence b^n = b^n/2 * b^n/2; run time log(n); however, when odd b^n = b^n/2 * b^n/2 * b; code below
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int powerB(int base, int exp) {
+    if (exp == 0)
+        return 1;
+    if (exp == 1)
+        return base;
+    if (exp%2 == 0) //if exp is even
+        return powerB(base*base, exp/2);
+    else //if exp is odd
+        return powerB(base*base, exp/2) *base;
+}
+
+
+int main() {
+    int base, exponent;
+    printf("Input base and exponent (2 3): \n");
+    scanf("%d%d", base, exponent);
+    printf("%d", powerB(base,exponent));
+
+    return 0;
+}
