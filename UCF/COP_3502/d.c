dec54853e17a5117051cfccec9f0a33546c78633
KashyapBathina <100645835+KashyapBathina@users.noreply.github.com>
2024-02-15 06:33:45 +0000

    Thu, Feb 15, 2024, 6:33 AM +00:00

--- UCF/COP_3502/d.c
+++ UCF/COP_3502/d.c
@@ -1,5 +1,5 @@
 /*
 recursion:
     body of function calls func itself; however, must make breaking point to prevent infinite loop; ie, can call f2 inside f1 or f1 inside f1
-    example: void rec2(int x) {if x==0 return;   rec2(x-1); printf("%d", x)}, prints 1,2,3 . . . bc printf kept in stack
+    example: void rec2(int x) {if x==0 return;   rec2(x-1); printf("%d", x)}, prints 1,2,3 . . . bc printf kept in call stack
 */
